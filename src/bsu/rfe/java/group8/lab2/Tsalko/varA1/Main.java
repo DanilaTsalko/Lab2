5542e92c9f6ca01f00d042b8e3767b4d873b4610
Danila <calko.danila@gmail.com>
2023-12-19 12:24:37 +0300

    first

---
+++ src/bsu/rfe/java/group8/lab2/Tsalko/varA1/Main.java
@@ -0,0 +1,12 @@
+package bsu.rfe.java.group8.lab2.Tsalko.varA1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MainFrame calc = new MainFrame();
+
+        calc.setVisible(true);
+
+
+    }
+}
